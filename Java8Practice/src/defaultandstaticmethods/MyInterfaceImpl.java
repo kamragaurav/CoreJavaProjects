100fa05262085d75af081e3f06415f2bd01e92a3
gaurav <kamra.gaurav515@gmail.com>
2019-04-02 23:43:38 +0530

    hl

--- Java8Practice/src/defaultandstaticmethods/MyInterfaceImpl.java
+++ Java8Practice/src/defaultandstaticmethods/MyInterfaceImpl.java
@@ -1,18 +1,25 @@
 package defaultandstaticmethods;
 
-public class MyInterfaceImpl implements MyInterface{
+public class MyInterfaceImpl implements MyInterface,MyInterface1{
 	
 	public static void main(String[] args) {
 		MyInterfaceImpl impl = new MyInterfaceImpl();
+		
 		impl.printHello();
 		impl.method1();
-		MyInterface.printBye();
+		
 	}
 
 	@Override
 	public void method1() {
 		// TODO Auto-generated method stub
 		System.out.println("Hiiii");
+	}
+
+	@Override
+	public void printHello() {
+		// TODO Auto-generated method stub
+		MyInterface1.super.printHello();
 	}
 
 }
