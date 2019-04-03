41d9d725fc101a33df7477c604d1b2a60926e5e6
gaurav.kamra <gaurav.kamra@YP1000526LT.Yash.com>
2019-04-03 21:35:59 +0530

    ol

--- Java8Practice/src/com/stream/mapper/StreamAPIMapTest.java
+++ Java8Practice/src/com/stream/mapper/StreamAPIMapTest.java
@@ -4,8 +4,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BinaryOperator;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
